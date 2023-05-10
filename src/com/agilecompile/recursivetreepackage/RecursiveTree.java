6631e5a6469e8b6ac000fd0870e5a77066c79e12
VimDeus <90948983+VimDeus@users.noreply.github.com>
2023-05-09 22:31:16 -0400

    Init of Function

---
+++ src/com/agilecompile/recursivetreepackage/RecursiveTree.java
@@ -0,0 +1,62 @@
+package com.agilecompile.recursivetreepackage;
+
+import java.nio.file.DirectoryStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.attribute.BasicFileAttributes;
+import java.util.Scanner;
+
+public class RecursiveTree {
+
+	public static void main(String[] args) throws Exception {
+	
+		Scanner scanner = new Scanner(System.in);
+	    System.out.println("Please enter a directory path:");
+	    String directoryPath = scanner.nextLine();
+
+	    try {
+	        Path currentPath = Paths.get(directoryPath);
+	        System.out.println("Listing files in directory: " + currentPath);
+	        listDirectory(currentPath, 0);
+	    } catch (Exception e) {
+	        e.printStackTrace();
+	    }
+	}
+	
+	//Recursive method to list Directory
+	public static void listDirectory(Path path, int depth) throws Exception {
+		
+		//Retrieval of file attributes into attr
+		BasicFileAttributes attr = Files.readAttributes(path, BasicFileAttributes.class);
+		
+		
+		// if "attr" is a directory then..
+		if(attr.isDirectory()) {
+			
+			//
+			DirectoryStream<Path> paths = Files.newDirectoryStream(path);
+			
+			System.out.println(spacesForDepth(depth) + " > " + path.getFileName());
+			
+			//Recursion of listDirectory 
+			for(Path tempPath: paths) {
+				listDirectory(tempPath, depth + 1);
+			}
+			
+		} else { 
+			
+			//Once there is no directory, print all files within the parent directory. 
+			System.out.println(spacesForDepth(depth) + " -- " + path.getFileName());
+		}
+	}
+	
+	//Method used to make spaces indicating parent-child relative tree depth
+	public static String spacesForDepth(int depth) {
+		StringBuilder builder = new StringBuilder();
+		for(int i = 0; i < depth; i++) {
+			builder.append("   ");
+		}
+		return builder.toString();
+	}
+}
